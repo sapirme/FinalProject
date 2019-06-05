c970d66e712cc249b0d3fb66ab284a7e8d29428d
Adar93 <adarne@post.bgu.ac.il>
2019-06-05 12:41:11 +0300

    Merge branch 'Adar'
    # Conflicts:
    #	mxgraph/javascript/examples/grapheditor/www/js/Actions.js

--- DAL/DAL/DAL_Interface.java
+++ DAL/DAL/DAL_Interface.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public interface DAL_Interface {
      boolean InsertObject(String D3, String SVG, String XML, int circles1, int lines1, int circles2, int lines2, String id_token);
      List<String> getAllIDs();
+     List<String> getMyIDs(String email);
      List<String> getNext8(List<String> IDs);
      Map<String, Pair<Integer, Integer>> getAllViewPoints(); // return map of vp's id and vp's shapes
      List<String> getObjIDByViewPointID(List<String> vpIDs);
